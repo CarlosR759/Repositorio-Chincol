47ee14dd9d8b338117c35fab39f111ef3fa9bd0c
CarlosR759 <carlos.uni.pucv@gmail.com>
2021-03-26 17:15:43 +0000

    mensaje

---
+++ Repositorio-Chincol/jose.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void){
+    //Tienen que imrpimir algo por acá para saber que les funciona editar//
+
+    return 0;
+}
---
+++ Repositorio-Chincol/rocio.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void){
+    //Tienen que imprimir lo que quieran acá para saber que pueden editar//
+
+    return 0;
+}
