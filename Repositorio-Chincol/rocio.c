8b1b1aafca647b8b7ccdec685418db3c6417d930
ribrl <81048589+ribrl@users.noreply.github.com>
2021-03-28 18:18:28 -0300

    Update rocio.c

--- Repositorio-Chincol/rocio.c
+++ Repositorio-Chincol/rocio.c
@@ -2,6 +2,8 @@
 
 int main(void){
     //Tienen que imprimir lo que quieran acá para saber que pueden editar//
+    printf("Hola mundo");
+    printf("Acabo de ver lo que hicieron y les quedó super!");
 
     return 0;
 }
